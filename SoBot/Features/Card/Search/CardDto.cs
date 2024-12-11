359070740904da14a9b840d4d33c2791d2ffd6e0
XenotropicDev <eliotd@gmail.com>
2024-12-11 12:01:27 -0800

    refactor: move card dto to models

--- SoBot/Features/Card/Search/CardDto.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Dominic Eliot.  All rights reserved.
-
-namespace SorceryBot.Features.Card.Search;
-
-public record CardDto
-{
-}
---
+++ SoBot/Models/Card.cs
@@ -0,0 +1,60 @@
+﻿namespace SorceryBot.Models;
+
+public class Card
+{
+    public string Name { get; set; }
+    public Guardian Guardian { get; set; }
+    public string Elements { get; set; }
+    public string SubTypes { get; set; }
+    public IEnumerable<Set> Sets { get; set; }
+}
+
+public class Guardian
+{
+    public string Rarity { get; set; }
+    public string Type { get; set; }
+    public string RulesText { get; set; }
+    public int? Cost { get; set; }
+    public int? Attack { get; set; }
+    public int? Defence { get; set; }
+    public int? Life { get; set; }
+    public Thresholds Thresholds { get; set; }
+}
+
+public class Thresholds
+{
+    public int Air { get; set; }
+    public int Earth { get; set; }
+    public int Fire { get; set; }
+    public int Water { get; set; }
+}
+
+public class Set
+{
+    public string Name { get; set; }
+    public DateTime ReleasedAt { get; set; }
+    public Metadata Metadata { get; set; }
+    public IEnumerable<Variant> Variants { get; set; }
+}
+
+public class Metadata
+{
+    public string Rarity { get; set; }
+    public string Type { get; set; }
+    public string RulesText { get; set; }
+    public int? Cost { get; set; }
+    public int? Attack { get; set; }
+    public int? Defence { get; set; }
+    public int? Life { get; set; }
+    public Thresholds Thresholds { get; set; }
+}
+
+public class Variant
+{
+    public string Slug { get; set; }
+    public string Finish { get; set; }
+    public string Product { get; set; }
+    public string Artist { get; set; }
+    public string FlavorText { get; set; }
+    public string TypeText { get; set; }
+}
