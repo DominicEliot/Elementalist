2ec127a33c70ca652494fab874e803cc96970129
XenotropicDev <eliotd@gmail.com>
2024-12-11 12:01:20 -0800

    feat: add logging pipeline

--- SoBot/Features/Card/Search/CardSearchQuery.cs
+++ SoBot/Features/Card/Search/CardSearchQuery.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MediatR;
+using SorceryBot.Shared;
 
 namespace SorceryBot.Features.Card.Search;
-public record CardSearchQuery : IRequest<IEnumerable<CardDto>>
+public record CardSearchQuery : IQuery<IEnumerable<CardDto>>
 {
 }
--- SoBot/Program.cs
+++ SoBot/Program.cs
@@ -2,6 +2,8 @@ using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using Serilog;
+using SorceryBot.Features.Card.Search;
+using SorceryBot.Infrastructure.Logging;
 
 namespace SorceryBot;
 
@@ -19,6 +21,11 @@ public class Program
         builder.Services.AddSerilog(Log.Logger);
 
         builder.Services.AddSingleton<IDiscordClient, DiscordSocketClient>();
+
+        builder.Services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssemblyContaining<CardSearchQuery>();
+            cfg.AddOpenBehavior(typeof(QueryLoggingPipeline<,>));
+        });
 
         var host = builder.Build();
         host.Run();
