fce92b90f99a6eb4b7299fff39eabe7cf3e2118e
XenotropicDev <eliotd@gmail.com>
2024-12-11 12:01:26 -0800

    test: add test for query handler

---
+++ SorceryBotTests/Features/Card/Search/CardSearchQueryHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SorceryBot.Features.Card.Search;
+using SorceryBot.Infrastructure.DataAccess.CardData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorceryBot.Features.Card.Search.Tests;
+
+[TestFixture()]
+public class CardSearchQueryHandlerTests
+{
+    [Test()]
+    public async Task HandleTestAsync()
+    {
+        var query = new CardSearchQuery() {CardQuery = "Pudge" };
+        var handler = new CardSearchQueryHandler(new FileCardRepository());
+
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        Assert.That(result, Is.Not.Null);
+    }
+}
---
+++ SorceryBotTests/Infrastructure/DataAccess/CardData/CuriosaApiCardRepositoryTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace SorceryBot.Infrastructure.DataAccess.CardData.Tests;
+
+[TestFixture()]
+public class CuriosaApiCardRepositoryTests
+{
+    [Test()]
+    public async Task GetCardsTestAsync()
+    {
+        Assert.Fail();
+    }
+}
