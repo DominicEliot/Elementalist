9b48c68a633c2d4c3937bf6b16a347d5829e19c9
XenotropicDev <eliotd@gmail.com>
2025-01-27 06:30:22 -1000

    feat: prototype results class

---
+++ SoBot/Shared/Result.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+
+namespace SorceryBot.Shared;
+public class Result<TResult>
+{
+    public TResult? Value { get; }
+    public Error? Error { get; }
+
+    public bool IsError() => Error != null;
+    public bool IsValid() => Value != null;
+}
+
+public class Error
+{
+}
