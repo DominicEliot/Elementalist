e43e97541338b3da90e4370d0a46293409d57674
XenotropicDev <eliotd@gmail.com>
2025-02-15 08:29:11 -0800

    feat: results pattern implementation

--- SoBot/Shared/Result.cs
+++ SoBot/Shared/Result.cs
@@ -3,13 +3,32 @@
 namespace SorceryBot.Shared;
 public class Result<TResult>
 {
-    public TResult? Value { get; }
+    private readonly TResult? _value;
+
+    public TResult Value => _value ?? throw new InvalidOperationException($"Make sure to check if there's an error before accessing the {nameof(Value)} property");
     public Error? Error { get; }
 
     public bool IsError() => Error != null;
     public bool IsValid() => Value != null;
+
+    public Result(Error error)
+    {
+        Error = error;
+    }
+
+    public Result(TResult value)
+    {
+        _value = value;
+    }
 }
 
 public class Error
+{
+    public Error() { Message = "An error occoured"; }
+    public Error(string message) { Message = message; }
+    public string Message { get; }
+}
+
+public class NotFoundError() : Error("The requested record could not be found")
 {
 }
