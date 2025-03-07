5425c53610c6a2391c0e4ad39a3f723dab6490f0
XenotropicDev <eliotd@gmail.com>
2025-03-07 13:05:51 -0800

    feat: add emoji's for threshold and mana values

--- SoBot/DiscordUi/DiscordLookups.cs
+++ SoBot/DiscordUi/DiscordLookups.cs
@@ -2,7 +2,7 @@
 
 namespace SorceryBot.DiscordUi;
 
-internal static class DiscordLookups
+public static class DiscordLookups
 {
     internal static Color GetCardColor(string elements)
     {
@@ -23,5 +23,22 @@ internal static class DiscordLookups
 
         // Colorless
         return new Color(0xdcdcdc);
+    }
+
+    public static string ReplaceManaTokensWithEmojis(string input)
+    {
+        input = input.Replace("(A)", "<:t_air:1347668260157526046>");
+        input = input.Replace("(W)", "<:t_water:1347668265463447693>");
+        input = input.Replace("(E)", "<:t_earth:1347668261642440827>");
+        input = input.Replace("(F)", "<:t_fire:1347668263806697472>");
+        input = input.Replace("(0)", "<:m_00:1347668233116713093>");
+        input = input.Replace("(1)", "<:m_01:1347668234614079569>");
+        input = input.Replace("①", "<:m_01:1347668234614079569>");
+        input = input.Replace("(2)", "<:m_02:1347668236824612985>");
+        input = input.Replace("(3)", "<:m_03:1347668238418579638>");
+        input = input.Replace("(4)", "<:m_04:1347668240364601354>");
+        input = input.Replace("(X)", "<:m_X:1347668251500609546>");
+
+        return input;
     }
 }
