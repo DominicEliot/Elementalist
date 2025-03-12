a8827179824b983b60479b0a4cc5f27ad33c3bbf
XenotropicDev <eliotd@gmail.com>
2025-03-12 13:25:00 -0700

    test: add mock card repo

---
+++ SorceryBotTests/Features/Card/MockCardRepository.cs
@@ -0,0 +1,20 @@
+﻿using SorceryBot.Infrastructure.DataAccess.CardData;
+
+namespace SorceryBotTests.Features.Card;
+
+internal class MockCardRepository : ICardRepository
+{
+    private List<Models.Card> _cards = [
+        new() { Name = "Pudge Butcher"}
+        ];
+
+    public Task<IEnumerable<Models.Card>> GetCards()
+    {
+        return Task.FromResult(_cards.AsEnumerable());
+    }
+
+    public Task<IEnumerable<Models.Card>> GetCardsMatching(Func<Models.Card, bool> predicate)
+    {
+        return Task.FromResult(_cards.Where(predicate));
+    }
+}
