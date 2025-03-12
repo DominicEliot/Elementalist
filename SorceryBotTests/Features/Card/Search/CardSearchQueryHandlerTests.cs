be2674b6047da25e9982b338448e2b5f46102da2
XenotropicDev <eliotd@gmail.com>
2025-03-12 13:24:44 -0700

    test: use mock card repository

--- SorceryBotTests/Features/Card/Search/CardSearchQueryHandlerTests.cs
+++ SorceryBotTests/Features/Card/Search/CardSearchQueryHandlerTests.cs
@@ -1,13 +1,4 @@
-﻿using SorceryBot.Features.Card.Search;
-using SorceryBot.Infrastructure.DataAccess.CardData;
-using SorceryBot.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace SorceryBot.Features.Card.Search.Tests;
 
@@ -16,8 +7,8 @@ public class CardSearchQueryHandlerTests
     [Fact]
     public async Task HandleTestAsync()
     {
-        var query = new GetCardsQuery() {CardNameContains = "Pudge" };
-        var handler = new GetCardsQueryHandler(new FileCardRepository());
+        var query = new GetCardsQuery() { CardNameContains = "Pudge" };
+        var handler = new GetCardsQueryHandler(new MockCardRepository());
 
         var result = await handler.Handle(query, CancellationToken.None);
 
