9dc3455311cd91ca618b398681ea21067d597a32
XenotropicDev <eliotd@gmail.com>
2025-04-08 07:14:37 -0700

    feat: wait for connect

--- Elementalist/BotActivityChangingService.cs
+++ Elementalist/BotActivityChangingService.cs
@@ -3,24 +3,56 @@ using NetCord.Gateway;
 
 namespace Elementalist;
 
-public class BotActivityChangingService(GatewayClient client, IOptions<ActivityOptions> options) : BackgroundService
+public class BotActivityChangingService : BackgroundService
 {
+    private readonly GatewayClient _client;
+    private readonly IOptions<ActivityOptions> _options;
+    private bool _connected = false;
+
+    public BotActivityChangingService(GatewayClient client, IOptions<ActivityOptions> options)
+    {
+        _client = client;
+        _options = options;
+        _client.Connect += _client_Connect;
+    }
+
+    private ValueTask _client_Connect()
+    {
+        _connected = true;
+        _client.Connect -= _client_Connect;
+        _client.Disconnect += _client_Disconnect;
+        return ValueTask.CompletedTask;
+    }
+
+    private ValueTask _client_Disconnect(bool arg)
+    {
+        _connected = false;
+        _client.Connect += _client_Connect;
+        _client.Disconnect -= _client_Disconnect;
+        return ValueTask.CompletedTask;
+    }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            var rand = Random.Shared.Next(0, options.Value.Activities.Count());
+            if (!_connected)
+            {
+                await Task.Delay(1000);
+            }
 
-            var status = options.Value.Activities.ElementAt(rand);
+            var rand = Random.Shared.Next(0, _options.Value.Activities.Count());
+
+            var status = _options.Value.Activities.ElementAt(rand);
 
             try
             {
-                await client.UpdatePresenceAsync(new PresenceProperties(NetCord.UserStatusType.Online)
+                await _client.UpdatePresenceAsync(new PresenceProperties(NetCord.UserStatusType.Online)
                 {
                     Activities = [new UserActivityProperties(status, UserActivityType.Custom)]
                 }, cancellationToken: stoppingToken);
 
-                await Task.Delay(TimeSpan.FromMinutes(options.Value.CycleTimeInMinutes), stoppingToken);
+                await Task.Delay(TimeSpan.FromMinutes(_options.Value.CycleTimeInMinutes), stoppingToken);
             }
             catch (OperationCanceledException)
             {
