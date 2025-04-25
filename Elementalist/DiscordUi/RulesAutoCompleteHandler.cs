1c860d6270767a5f82761ef390e3a5068b5bc76b
XenotropicDev <eliotd@gmail.com>
2025-04-24 18:38:30 -0700

    feat: json rules repo, and auto complete handler

---
+++ Elementalist/DiscordUi/RulesAutoCompleteHandler.cs
@@ -0,0 +1,27 @@
+﻿using Elementalist.Infrastructure.DataAccess.CardData;
+using ElementalistBot.Infrastructure.DataAccess.Rules;
+using NetCord;
+using NetCord.Rest;
+using NetCord.Services.ApplicationCommands;
+
+namespace Elementalist.DiscordUi;
+
+public class RulesAutoCompleteHandler(RulesRepository rulesRepository) : IAutocompleteProvider<AutocompleteInteractionContext>
+{
+    private readonly RulesRepository rulesRepository = rulesRepository;
+
+    public async ValueTask<IEnumerable<ApplicationCommandOptionChoiceProperties>?> GetChoicesAsync(ApplicationCommandInteractionDataOption option, AutocompleteInteractionContext context)
+    {
+        var value = option.Value;
+
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return [new("Airborne", "Airborne"), new("Immobile", "Immobile/Immobilised"), new("Disabled", "Disable/Disabled")];
+        }
+
+        var searchParams = value.Split(' ', '-', '&');
+
+        var suggestions = (await rulesRepository.GetRules()).Where(rule => searchParams.Any(sp => rule.Name.Contains(sp, StringComparison.OrdinalIgnoreCase)));
+        return suggestions.Take(25).Select(c => new ApplicationCommandOptionChoiceProperties(c.Name, c.Name));
+    }
+}
--- Elementalist/Infrastructure/DataAccess/Rules/RulesRepository.cs
+++ Elementalist/Infrastructure/DataAccess/Rules/RulesRepository.cs
@@ -1,10 +1,20 @@
-﻿using ElementalistBot.Models;
+﻿using System.Text.Json;
+using ElementalistBot.Models;
 
 namespace ElementalistBot.Infrastructure.DataAccess.Rules;
+
 public class RulesRepository
 {
-    public Task<IEnumerable<Rule>> GetRules()
+    private IEnumerable<Rule> _rules = [];
+
+    public async Task<IEnumerable<Rule>> GetRules()
     {
-        return Task.FromResult(new List<Rule>().AsEnumerable());
+        if (!_rules.Any())
+        {
+            var jsonFile = await File.ReadAllTextAsync(Path.Combine("rules.json"));
+            _rules = JsonSerializer.Deserialize<IEnumerable<Rule>>(jsonFile) ?? [];
+        }
+
+        return _rules;
     }
 }
