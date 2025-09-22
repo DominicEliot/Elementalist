2008656ecb7c0fecdb370acca2ead9bd3c2e041e
DominicEliot <eliotd@gmail.com>
2025-09-22 15:04:10 -0700

    logging: add log entry for fetching cards from curiosa

--- Elementalist/Infrastructure/DataAccess/CardData/ICardRepository.cs
+++ Elementalist/Infrastructure/DataAccess/CardData/ICardRepository.cs
@@ -12,20 +12,23 @@ public interface ICardRepository
     Task<IEnumerable<Card>> GetCardsMatching(Func<Card, bool> predicate);
 }
 
-public class CuriosaApiCardRepository(HttpClient httpClient) : ICardRepository
+public class CuriosaApiCardRepository(HttpClient httpClient, ILogger<ICardRepository> logger) : ICardRepository
 {
     private readonly HttpClient _httpClient = httpClient;
+    private readonly ILogger<ICardRepository> _logger = logger;
     private List<Card> _cards = [];
-    private DateTimeOffset _lastUpdated;
+    private DateTimeOffset _lastUpdated = DateTimeOffset.MinValue;
 
     public async Task<IEnumerable<Card>> GetCards()
     {
         if (_lastUpdated < SystemClock.Now.AddHours(48))
         {
+            _logger.LogInformation("Fetching cards from Curiosa API");
             var cardsFromApi = await _httpClient.GetAsync("https://api.sorcerytcg.com/api/cards");
             var cardResults = await cardsFromApi.Content.ReadFromJsonAsync<List<Card>>();
             _cards = cardResults ?? [];
             _lastUpdated = SystemClock.Now;
+            _logger.LogInformation($"Fetched {_cards.Count} cards from Curiosa API");
         }
 
         return _cards;
