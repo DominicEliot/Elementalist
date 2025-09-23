9a75ce1a1bd67dab2222298abda3027ec0eaef99
DominicEliot <eliotd@gmail.com>
2025-09-23 10:56:05 -0700

    feat: add data refresh options

--- ElementalistTests/Infrastructure/DataAccess/CardData/CuriosaApiCardRepositoryTest.cs
+++ ElementalistTests/Infrastructure/DataAccess/CardData/CuriosaApiCardRepositoryTest.cs
@@ -1,5 +1,6 @@
 using Elementalist.Infrastructure.DataAccess.CardData;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace ElementalistTests.Infrastructure.DataAccess.CardData;
@@ -12,9 +13,9 @@ public class CuriosaApiCardRepositoryTest
     public async Task GetCardsTest()
     {
         using var client = new HttpClient();
-        using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+        var options = Options.Create(new DataRefreshOptions { Hours = 48 });
 
-        var api = new CuriosaApiCardRepository(client, loggerFactory.CreateLogger<ICardRepository>());
+        var api = new CuriosaApiCardRepository(client, options);
 
         var cards = await api.GetCardsMatching(c => c.Name.Contains("Dragonlord", StringComparison.OrdinalIgnoreCase));
 
