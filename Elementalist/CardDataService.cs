f00cfc983767ef025dc734b0bf69345cb2af17a8
DominicEliot <eliotd@gmail.com>
2025-09-23 06:34:50 -0700

    feat: add card data auto fetching service

---
+++ Elementalist/CardDataService.cs
@@ -0,0 +1,27 @@
+﻿using Elementalist.Infrastructure.DataAccess.CardData;
+using Serilog;
+using static Elementalist.Features.Cards.Prices;
+
+namespace Elementalist;
+
+public class CardDataService(ICardRepository tcgPlayerData) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Log.Information("Fetching card data.");
+            var cards = await tcgPlayerData.GetCards();
+            Log.Information("Loaded {count} cards.", cards.Count());
+
+            try
+            {
+                await Task.Delay(TimeSpan.FromHours(47.5), stoppingToken);
+            }
+            catch (TaskCanceledException)
+            {
+                break;
+            }
+        }
+    }
+}
