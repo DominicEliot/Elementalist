49c554f09606dda04ab51a1ad95621f7362f4506
DominicEliot <eliotd@gmail.com>
2025-12-24 10:38:02 -0800

    refactor: rename class to match functionality

--- Elementalist/Features/Rules/GetRules.cs
+++ Elementalist/Features/Rules/GetRules.cs
@@ -6,12 +6,11 @@ using MediatR;
 namespace ElementalistBot.Features.Rules;
 public record GetRulesQuery(string RuleName) : IQuery<IEnumerable<CodexEntry>>;
 
-public class GetCardsQueryHandler(CodexCsvRulesRepository rulesRepository) : IRequestHandler<GetRulesQuery, IEnumerable<CodexEntry>>
+public class GetRulesQueryHandler(CodexCsvRulesRepository rulesRepository) : IRequestHandler<GetRulesQuery, IEnumerable<CodexEntry>>
 {
     public async Task<IEnumerable<CodexEntry>> Handle(GetRulesQuery request, CancellationToken cancellationToken)
     {
-        var rules = (await rulesRepository.GetRules()).Where(r => r.Title.Equals(request.RuleName, StringComparison.OrdinalIgnoreCase));
-
-        return rules;
+        return (await rulesRepository.GetRules())
+            .Where(r => r.Title.Equals(request.RuleName, StringComparison.OrdinalIgnoreCase));
     }
 }
