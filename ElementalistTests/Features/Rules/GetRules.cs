c0b0a2e41bead5e1b598f7be1bcf7eb962710658
DominicEliot <eliotd@gmail.com>
2025-12-31 12:53:00 -0800

    chore: update namespace

--- ElementalistTests/Features/Rules/GetRules.cs
+++ ElementalistTests/Features/Rules/GetRules.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ElementalistBot.Infrastructure.DataAccess.Rules;
+﻿using Elementalist.Infrastructure.DataAccess.Rules;
 using Xunit;
 
 namespace ElementalistTests.Features.Rules;
