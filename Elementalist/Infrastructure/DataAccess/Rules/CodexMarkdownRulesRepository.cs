b4eb04414cea3ad819e15849d49137070074f5bd
Dominic Eliot <deliot@dmv.nv.gov>
2026-02-16 08:33:46 -0800

    feat: initial stub

---
+++ Elementalist/Infrastructure/DataAccess/Rules/CodexMarkdownRulesRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Elementalist.Models;
+
+namespace Elementalist.Infrastructure.DataAccess.Rules;
+
+internal partial class CodexMarkdownRulesRepository : IRulesRepository
+{
+    public Task<IEnumerable<string>> GetKeywords()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<CodexEntry>> GetRules()
+    {
+        var markDown = await File.ReadAllTextAsync("Full Codex.md");
+        var markdownDirectory = new DirectoryInfo("");
+        foreach(var file in markdownDirectory.EnumerateFiles(""))
+        {
+
+        }
+
+        var splits = LineStartsWithH1Regex().Split(markDown);
+
+        var codex = new List<CodexEntry>();
+        foreach (var singleMarkdown in splits)
+        {
+            if (string.IsNullOrWhiteSpace(singleMarkdown))
+            {
+                continue;
+            }
+
+            var title = GroupedCodexContentRegex().Match(singleMarkdown);
+            var entry = new CodexEntry { Content = singleMarkdown, Title = };
+        }
+    }
+
+    [GeneratedRegex(@"^(?=# )")]
+    private static partial Regex LineStartsWithH1Regex();
+    [GeneratedRegex(@"^# (.*)\n(.*)")]
+    private static partial Regex GroupedCodexContentRegex();
+}
